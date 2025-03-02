f2dd4a6d9ba806b180802146572d58e6f7d2b0e1
ecra2001 <ecra6157@byu.edu>
2025-03-01 19:17:54 -0700

    working on MemoryUserDAO.java

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,4 +1,30 @@
 package dataaccess;
 
 import model.UserData;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryUserDAO implements UserDAO {
+    private final Map<String, UserData> db = new HashMap<>();
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        if (db.containsKey(user.getUsername())) {
+            throw new DataAccessException("User already exists.");
+        }
+        db.put(user.getUsername(), user);
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+    }
+
+    @Override
+    public boolean authUser(String username, String password) throws DataAccessException {
+    }
+
+    @Override
+    public void clear() {
+        db.clear();
+    }
+}
--- shared/src/main/java/model/UserData.java
+++ shared/src/main/java/model/UserData.java
@@ -2,12 +2,24 @@ package model;
 
 public class UserData {
     private final String username;
+    private final String password;
+    private final String email;
 
-    UserData(String username, String password, String email) {
+    public UserData(String username, String password, String email) {
         this.username = username;
+        this.password = password;
+        this.email = email;
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
     }
 }
