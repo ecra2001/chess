b96b9437c3b8044e1006fbb646ee10efff8ee88e
ecra2001 <ecra6157@byu.edu>
2025-03-03 14:21:31 -0700

    setting up Service Tests

--- server/src/test/java/service/ServiceTest.java
+++ server/src/test/java/service/ServiceTest.java
@@ -4,6 +4,7 @@ import dataaccess.*;
 import model.AuthData;
 import model.UserData;
 import model.GameData;
+import org.junit.jupiter.api.Test;
 import service.Service;
 
 public class ServiceTest {
@@ -12,4 +13,57 @@ public class ServiceTest {
     static GameDAO gameDAO;
     static Service.UserService userService;
     static Service.GameService gameService;
+
+    @Test
+    void testCreateUserPositive() throws DataAccessException {
+
+    }
+    @Test
+    void testCreateUserNegative() throws DataAccessException {
+
+    }
+    @Test
+    void testLoginUserPositive() throws DataAccessException {
+
+    }
+    @Test
+    void testLoginUserNegative() throws DataAccessException {
+
+    }
+    @Test
+    void testLogoutUserPositive() throws DataAccessException {
+
+    }
+    @Test
+    void testLogoutUserNegative() throws DataAccessException {
+
+    }
+    @Test
+    void testClear() throws DataAccessException {
+
+    }
+    @Test
+    void testListGamesPositive() throws DataAccessException {
+
+    }
+    @Test
+    void testListGamesNegative() throws DataAccessException {
+
+    }
+    @Test
+    void testCreateGamePositive() throws DataAccessException {
+
+    }
+    @Test
+    void testCreateGameNegative() throws DataAccessException {
+
+    }
+    @Test
+    void testJoinGamePositive() throws DataAccessException {
+
+    }
+    @Test
+    void testJoinGameNegative() throws DataAccessException {
+
+    }
 }
