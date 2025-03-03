cf5b64e99326408d75a95eb7e5a0b304bf71c3e4
ecra2001 <ecra6157@byu.edu>
2025-03-02 22:36:38 -0700

    creating Handler

---
+++ server/src/main/java/server/Handler.java
@@ -0,0 +1,15 @@
+package server;
+
+import model.GameData;
+import model.UserData;
+import model.AuthData;
+import java.util.HashSet;
+
+public class Handler {
+    public static class UserHandler {
+
+    }
+    public static class GameHandler {
+
+    }
+}
