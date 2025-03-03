2c1a44ee16b95bf6bd0dc07b65fe8a8bc7ed623f
ecra2001 <ecra6157@byu.edu>
2025-03-02 22:58:28 -0700

    structuring Handler

--- server/src/main/java/server/Handler.java
+++ server/src/main/java/server/Handler.java
@@ -1,15 +1,52 @@
 package server;
 
+import dataaccess.DataAccessException;
 import model.GameData;
 import model.UserData;
 import model.AuthData;
 import java.util.HashSet;
+import service.Service.UserService;
+import service.Service.GameService;
+import spark.Request;
+import spark.Response;
 
 public class Handler {
     public static class UserHandler {
+        UserService userService;
+        public UserHandler(UserService userService){
+            this.userService = userService;
+        }
+
+        public Object register(Request req, Response resp) throws DataAccessException {
+            return null;
+        }
+
+        public Object login(Request req, Response resp) throws DataAccessException {
+            return null;
+        }
+
+        public Object logout(Request req, Response resp) throws DataAccessException {
+            return null;
+        }
+
 
     }
     public static class GameHandler {
+        GameService gameService;
+        public GameHandler(GameService gameService){
+            this.gameService = gameService;
+        }
 
+        public Object listGames(Request req, Response resp) throws DataAccessException {
+            return null;
+        }
+
+        public Object createGame(Request req, Response resp) throws DataAccessException {
+            return null;
+        }
+
+        public Object joinGame(Request req, Response resp) throws DataAccessException {
+            return null;
+        }
     }
 }
