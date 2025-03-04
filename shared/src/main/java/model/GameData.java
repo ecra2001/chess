94f24fca6c8c08401587df52b4f425cb5fe51692
ecra2001 <ecra6157@byu.edu>
2025-03-03 23:00:14 -0700

    improved code readability

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -48,7 +48,10 @@ public class GameData {
             return false;
         }
         GameData gameData = (GameData) o;
-        return gameID == gameData.gameID && Objects.equals(whiteUsername, gameData.whiteUsername) && Objects.equals(blackUsername, gameData.blackUsername) && Objects.equals(gameName, gameData.gameName) && Objects.equals(game, gameData.game);
+        return gameID == gameData.gameID && Objects.equals(whiteUsername, gameData.whiteUsername)
+                && Objects.equals(blackUsername, gameData.blackUsername)
+                && Objects.equals(gameName, gameData.gameName)
+                && Objects.equals(game, gameData.game);
     }
 
     @Override
