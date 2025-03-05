bf93741d982cf57096a4b12547e3bcbb7d4926cc
ecra2001 <ecra6157@byu.edu>
2025-03-05 14:08:20 -0700

    finished models

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -1,6 +1,9 @@
 package model;
 
 import chess.ChessGame;
+
+import java.util.Objects;
+
 class GameData {
     private int gameID;
     private String whiteUsername;
@@ -21,5 +24,45 @@ class GameData {
 
     public String getWhiteUsername(){
         return whiteUsername;
+    }
+
+    public String getBlackUsername(){
+        return blackUsername;
+    }
+
+    public String getGameName(){
+        return gameName;
+    }
+
+    public ChessGame getGame(){
+        return game;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        GameData gameData = (GameData) o;
+        return gameID == gameData.gameID && Objects.equals(whiteUsername, gameData.whiteUsername) && Objects.equals(blackUsername, gameData.blackUsername) && Objects.equals(gameName, gameData.gameName) && Objects.equals(game, gameData.game);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameID, whiteUsername, blackUsername, gameName, game);
+    }
+
+    @Override
+    public String toString() {
+        return "GameData{" +
+                "gameID=" + gameID +
+                ", whiteUsername='" + whiteUsername + '\'' +
+                ", blackUsername='" + blackUsername + '\'' +
+                ", gameName='" + gameName + '\'' +
+                ", game=" + game +
+                '}';
     }
 }
