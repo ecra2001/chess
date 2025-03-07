4f66e3857888bf2982998a4c56d47575b57e374f
ecra2001 <ecra6157@byu.edu>
2025-03-07 14:54:47 -0700

    setting up server and handler

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,6 +10,13 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.delete("/name", this::clear);
+        Spark.post("/name", this::register);
+        Spark.post("/name", this::login);
+        Spark.delete("/name", this::logout);
+        Spark.get("/name", this::listGames);
+        Spark.post("/name", this::createGame);
+        Spark.put("/name", this::joinGame);
 
         //This line initializes the server and can be removed once you have a functioning endpoint
         Spark.init();
@@ -21,5 +28,33 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private Object register(Request req, Response res) {
+        return null;
+    }
+
+    private Object login(Request req, Response res) {
+        return null;
+    }
+
+    private Object logout(Request req, Response res) {
+        return null;
+    }
+
+    private Object listGames(Request req, Response res) {
+        return null;
+    }
+
+    private Object createGame(Request req, Response res) {
+        return null;
+    }
+
+    private Object joinGame(Request req, Response res) {
+        return null;
+    }
+
+    private Object clear(Request req, Response res) {
+        return null;
     }
 }
