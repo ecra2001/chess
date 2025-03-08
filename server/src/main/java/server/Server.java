56fb38625abec81e2fb93ab5c3d6039028c3dc2f
ecra2001 <ecra6157@byu.edu>
2025-03-07 20:36:42 -0700

    created createGame endpoint

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -112,7 +112,7 @@ public class Server {
         try {
             String authToken = req.headers("authorization");
             HashSet<GameData> gameList = gameService.listGames(authToken);
-            var json = new Gson().toJson(Map.of("Games", gameList));
+            var json = new Gson().toJson(Map.of("games", gameList));
             res.status(200);
             return json;
         } catch (DataAccessException e) {
@@ -128,7 +128,21 @@ public class Server {
 
     private Object createGame(Request req, Response res) {
         try {
-            return null;
+            String authToken = req.headers("authorization");
+            GameData gameData = new Gson().fromJson(req.body(), GameData.class);
+            if (gameData == null) {
+                var body = new Gson().toJson(Map.of("message", "Error: bad request"));
+                res.status(400);
+                return body;
+            }
+            int gameID = gameService.createGame(gameData.getGameName(), authToken);
+            var json = new Gson().toJson(Map.of("gameID", gameID));
+            res.status(200);
+            return json;
+        } catch (DataAccessException e) {
+            var body = new Gson().toJson(Map.of("message", "Error: unauthorized"));
+            res.status(401);
+            return body;
         } catch (Exception e) {
             res.status(500);
             var body = new Gson().toJson(Map.of("message", String.format("Error: %s", e.getMessage())));
@@ -138,7 +152,15 @@ public class Server {
 
     private Object joinGame(Request req, Response res) {
         try {
+            String authToken = req.headers("authorization");
+            Map<String, Integer> body = new Gson().fromJson(req.body(), Map.class);
+
+            boolean attempt = gameService.joinGame(authToken, 0, null);
             return null;
+        } catch (DataAccessException e) {
+            var body = new Gson().toJson(Map.of("message", "Error: unauthorized"));
+            res.status(401);
+            return body;
         } catch (Exception e) {
             res.status(500);
             var body = new Gson().toJson(Map.of("message", String.format("Error: %s", e.getMessage())));
