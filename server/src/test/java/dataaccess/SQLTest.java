cfb6873a2f309a25975bbc6644c955d928bf1d61
ecra2001 <ecra6157@byu.edu>
2025-03-10 16:47:38 -0600

    set up tests for user sql

---
+++ server/src/test/java/dataaccess/SQLTest.java
@@ -0,0 +1,49 @@
+package dataaccess;
+
+import model.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+class SQLTest {
+    private SQLUserDAO sqlUserDAO;
+    @BeforeEach
+    public void setup() {
+        sqlUserDAO = new SQLUserDAO();
+    }
+
+    @Test
+    void getUserPositive() {
+
+    }
+
+    @Test
+    void getUserNegative() {
+
+    }
+
+    @Test
+    void createUserPositive() {
+
+    }
+
+    @Test
+    void createUserNegative() {
+
+    }
+
+    @Test
+    void authUserPositive() {
+
+    }
+
+    @Test
+    void authUserNegative() {
+
+    }
+
+    @Test
+    void clearUserTest() {
+
+    }
+}
