0425a1a5480f3b4221853725970cc32c34200a09
ecra2001 <ecra6157@byu.edu>
2025-03-11 11:37:49 -0600

    setup sql game

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -2,10 +2,42 @@ package dataaccess;
 
 import model.GameData;
 
+import java.sql.SQLException;
 import java.util.HashSet;
 
 public class SQLGameDAO implements GameDAO {
+    public SQLGameDAO() {
+        configureDatabase();
+    }
+    private final String[] createStatements = {
+            """
+          CREATE TABLE IF NOT EXISTS game (
+          `gameID` int NOT NULL,
+          `whiteUsername` varchar(256),
+          `blackUsername` varchar(256),
+          `gameName` varchar(256),
+          `game` TEXT DEFAULT NULL,
+          PRIMARY KEY (`gameID`)
+          )
+    """
+    };
 
+    private void configureDatabase() {
+        try {
+            DatabaseManager.createDatabase();
+        } catch (DataAccessException ex) {
+            throw new RuntimeException(ex);
+        }
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
     @Override
     public HashSet<GameData> getGameList() {
         return null;
