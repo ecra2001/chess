740f0962b77daf4217feb4c99681378a83bb92ef
ecra2001 <ecra6157@byu.edu>
2025-03-21 17:47:51 -0600

    passed registerPositive

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -5,16 +5,25 @@ import org.junit.jupiter.api.*;
 import server.Server;
 import model.*;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
 public class ServerFacadeTests {
 
     private static Server server;
-    private ServerFacade serverFacade;
+    private static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
+        var url = "http://localhost:" + port;
         System.out.println("Started test HTTP server on " + port);
+        serverFacade = new ServerFacade(url);
     }
 
     @AfterAll
@@ -25,7 +34,10 @@ public class ServerFacadeTests {
 
     @Test
     public void registerPositive() throws ResponseException {
+        UserData userData = new UserData("player1", "password", "p1@email.com");
         //Assertions.assertTrue(serverFacade.register(new UserData("username", "password", "email")));
+        var authData = serverFacade.register(userData);
+        assertTrue(authData.getAuthToken().length() > 10);
     }
 
 }
