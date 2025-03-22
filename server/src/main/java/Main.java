7c4fd9c3014634da58321c0d04d449d5dc547b4f
ecra2001 <ecra6157@byu.edu>
2025-03-22 14:45:21 -0600

    implemented main server

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,16 @@
 import chess.*;
+import dataaccess.*;
+import server.*;
 
 public class Main {
-    public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+    public static void main(String[] args) throws DataAccessException {
+        try { DatabaseManager.createDatabase(); } catch (DataAccessException e) {
+            throw new DataAccessException("Could not create database");
+        }
+        System.out.println("♕ 240 Chess Server");
+        int port = 8080;
+        Server server = new Server();
+        server.run(port);
+
     }
 }
